ff87db58e04f9b78ac62138ce507286804a5fe93
HamzaAziz-1 <88493437+HamzaAziz-1@users.noreply.github.com>
2022-08-06 18:09:20 +0500

    forecast component

---
+++ weather-app/src/api.jsx
@@ -0,0 +1,13 @@
+export const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "93b1e5603amsh59ea7a85e9e7b36p16b210jsn35af5aa8750e",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "64947c79c1d4e70d9f7b75d16cd61dcd";
