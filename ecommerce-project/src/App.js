07f85c7b0949d960ebb48f75881f902b84a38f3f
HamzaAziz-1 <hamza.aziz8750@gmail.com>
2023-08-17 14:54:31 +0500

    Update App.js

--- ecommerce-project/src/App.js
+++ ecommerce-project/src/App.js
@@ -1,6 +1,6 @@
-import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import { Navbar, Sidebar, Footer } from './components'
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Navbar, Sidebar, Footer } from "./components";
 import {
   Home,
   About,
@@ -10,36 +10,36 @@ import {
   Cart,
   Error,
   Checkout,
-} from './pages';
+} from "./pages";
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <Switch>
-        <Route exact path='/' >
-          <Home/>
+        <Route exact path="/">
+          <Home />
         </Route>
-        <Route exact path='/about' >
-          <About/>
+        <Route exact path="/about">
+          <About />
         </Route>
-        <Route exact path='/cart' >
-          <Cart/>
+        <Route exact path="/cart">
+          <Cart />
         </Route>
-        <Route exact path='/products' >
-          <Products/>
+        <Route exact path="/products">
+          <Products />
         </Route>
-        <Route exact path='/products/:id' children={<SingleProduct />} />
-        <Route exact path='/checkout' >
-          <Checkout/>
+        <Route exact path="/products/:id" children={<SingleProduct />} />
+        <Route exact path="/checkout">
+          <Checkout />
         </Route>
-        <Route exact path='*' >
-          <Error/>
+        <Route exact path="*">
+          <Error />
         </Route>
-        
       </Switch>
+      <Footer />
     </Router>
-
-  )
+  );
 }
 
-export default App
+export default App;
