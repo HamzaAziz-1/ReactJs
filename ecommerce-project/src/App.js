fcc43c69b4349e7604cfb75be53b8bfad22ad541
HamzaAziz-1 <hamza.aziz8750@gmail.com>
2023-08-17 14:53:13 +0500

    setup routes

--- ecommerce-project/src/App.js
+++ ecommerce-project/src/App.js
@@ -1,9 +1,45 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { Navbar, Sidebar, Footer } from './components'
+import {
+  Home,
+  About,
+  SingleProduct,
+  PrivateRoute,
+  Products,
+  Cart,
+  Error,
+  Checkout,
+} from './pages';
 
 function App() {
-  return <h4>comfy sloth starter</h4>
+  return (
+    <Router>
+      <Switch>
+        <Route exact path='/' >
+          <Home/>
+        </Route>
+        <Route exact path='/about' >
+          <About/>
+        </Route>
+        <Route exact path='/cart' >
+          <Cart/>
+        </Route>
+        <Route exact path='/products' >
+          <Products/>
+        </Route>
+        <Route exact path='/products/:id' children={<SingleProduct />} />
+        <Route exact path='/checkout' >
+          <Checkout/>
+        </Route>
+        <Route exact path='*' >
+          <Error/>
+        </Route>
+        
+      </Switch>
+    </Router>
+
+  )
 }
 
 export default App
